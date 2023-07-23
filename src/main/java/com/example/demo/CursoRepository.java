0ebed428c3c4ed8689a677f9c34ca97d982ac28a
FrankJrr31 <134321340+FrankJrr31@users.noreply.github.com>
2023-07-23 21:10:42 +0000

    nuevo

---
+++ src/main/java/com/example/demo/CursoRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.PagingAndSortingRepository;
+//import org.springframework.data.repository.query.Param;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+//import java.util.List;
+
+//@RepositoryRestResource(collectionResourceRel = "carreras", path = "carreras")
+public interface CursoRepository extends CrudRepository<Curso, Integer> {
+    
+}
